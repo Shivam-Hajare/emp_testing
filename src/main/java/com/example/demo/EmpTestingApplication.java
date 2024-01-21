f297ad7106cd3c2ba8174df69df22e067c90f36a
Shivam Hajare <shivamhajare96@gmail.com>
2024-01-21 19:03:28 +0530

    adi

---
+++ src/main/java/com/example/demo/EmpTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpTestingApplication.class, args);
+	}
+
+}
